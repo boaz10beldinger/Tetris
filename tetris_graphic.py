2256d2c17ba0627096f46ffef13612a9d12e57eb
YA-AR <yael.arbel.41@gmail.com>
2020-03-26 12:39:56 +0200

    26/3

--- tetris_graphic.py
+++ tetris_graphic.py
@@ -7,6 +7,7 @@ SCORE_PER_ROW  = 100
 COMBO = 2
 
 
+# changes screen according to the shape located
 def add_shape(pos, screen, shape):
     pos_x = pos[0]
     pos_y = pos[1]
@@ -77,11 +78,9 @@ def main_loop(screen, shapes):
     print(next_shapes)
     print_screen(screen)
     column = int(input("column wanted: "))
-    rotation_num = int(input("how many times would you like to rotate?: "))
+    idx = int(input(" which version do you want?"))
     upcoming_shape = next_shapes.pop(0)
-    if rotation_num > 0:
-        for i in range(rotation_num):
-            upcoming_shape = rotate(upcoming_shape)
+    upcoming_shape = upcoming_shape[idx]
     if not (0 <= column < N and column + len(upcoming_shape) < N):
         print("oops")
         return 0
@@ -92,11 +91,9 @@ def main_loop(screen, shapes):
         print(next_shapes)
         print_screen(screen)
         column = int(input("column wanted: "))
-        rotation_num = int(input("how many times would you like to rotate?: "))
+        idx = int(input(" which version do you want?"))
         upcoming_shape = next_shapes.pop(0)
-        if rotation_num > 0:
-            for i in range(rotation_num):
-                upcoming_shape = rotate(upcoming_shape)
+        upcoming_shape = upcoming_shape[idx]
         if not (0 <= column < N and column + len(upcoming_shape) < N):
             print("oops")
             break
@@ -127,13 +124,41 @@ def check_for_rows_and_explode(screen):
 if __name__ == "__main__":
 
     s = [[0 for i in range(N)] for j in range(M)]
-    tetriminos = [[[1, 1, 1], [0, 1, 0]],
-                  [[2, 2, 2, 2]],
-                  [[3, 3], [3, 3]],
-                  [[0, 4, 4], [4, 4, 0]],
-                  [[5, 5, 0], [0, 5, 5]],
-                  [[6, 6, 6], [0, 0, 6]],
-                  [[7, 7, 7], [7, 0, 0]]]
+    tetriminos = {1: [[[1, 1, 1],
+                       [0, 1, 0]], [[0, 1, 0],
+                                    [1, 1, 1]], [[1, 0],
+                                                [1, 1],
+                                                [1, 0]], [[0, 1],
+                                                         [1, 1],
+                                                         [0, 1]]],
+                  2: [[2, 2, 2, 2], [[2],
+                                     [2],
+                                     [2],
+                                     [2]]],
+                  3: [[3, 3],
+                      [3, 3]],
+                  4: [[[0, 4, 4],
+                      [4, 4, 0]],  [[4, 0],
+                                    [4, 4],
+                                    [0, 4]]],
+                  5: [[[5, 5, 0],
+                      [0, 5, 5]], [[0, 5],
+                                [5, 5],
+                                [5, 0]]],
+                  6: [[[6, 6, 6],
+                      [0, 0, 6]], [[6, 0, 0],
+                                    [6, 6, 6]], [[6, 6],
+                                                 [6, 0],
+                                                [6, 0]], [[0, 6],
+                                                          [0, 6],
+                                                          [6, 6]]],
+                  7: [[[7, 7, 7],
+                      [7, 0, 0]], [[0, 0, 7],
+                                    [7, 7, 7]], [[7, 0],
+                                                 [7, 0],
+                                                [7, 7]], [[7, 7],
+                                                          [0, 7],
+                                                          [6, 7]]]}
 
     main_loop(s, tetriminos)
     print("end")
